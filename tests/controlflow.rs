79b43db4647573d555691e682754c0a961aa2ae5
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2025-03-26 18:50:32 +0000

    test: refactor foreach loop instructions for array and dictionary handling

--- tests/controlflow.rs
+++ tests/controlflow.rs
@@ -112,6 +112,11 @@ fn test_for_while_loop() {
 #[test]
 fn test_foreach_array_loop() {
 
+    let array = 0;
+    let index = 1;
+    let max = 2;
+    let item = 3;
+
     let program = Program {
         instructions: vec![
             // Create an array with 3 elements
@@ -119,30 +124,30 @@ fn test_foreach_array_loop() {
             Instruction::Push(Variant::Integer(2)),
             Instruction::Push(Variant::Integer(3)),
             Instruction::CreateArray(3),
-            Instruction::SetLocal(0),
+            Instruction::SetLocal(array),
 
             // Set i = 0
             Instruction::Push(Variant::Integer(0)),
-            Instruction::SetLocal(1),
+            Instruction::SetLocal(index),
 
             // Get the length of the array and store it in max
-            Instruction::GetLocal(0),
+            Instruction::GetLocal(array),
             Instruction::GetArrayLength,
-            Instruction::SetLocal(2),
+            Instruction::SetLocal(max),
 
             // while i < max
-            Instruction::GetLocal(1), // 10
-            Instruction::GetLocal(2),
+            Instruction::GetLocal(index), // 10
+            Instruction::GetLocal(max),
             Instruction::LessThan,
 
             // Jump if false
             Instruction::JumpIfFalse(23), // 13
 
             // Get the array
-            Instruction::GetLocal(0),
-            Instruction::GetLocal(1),
+            Instruction::GetLocal(array),
+            Instruction::GetLocal(index),
             Instruction::GetArrayItem,
-            Instruction::SetLocal(3),
+            Instruction::SetLocal(item),
 
             // Increment i
             Instruction::GetLocal(1),
@@ -154,7 +159,7 @@ fn test_foreach_array_loop() {
             Instruction::Jump(10),
 
             // Return i
-            Instruction::GetLocal(1),
+            Instruction::GetLocal(index),
             Instruction::Return // 23
 
         ],
@@ -171,6 +176,15 @@ fn test_foreach_array_loop() {
 #[test]
 fn test_foreach_dictionary_loop() {
 
+    //TermLogger::init(LevelFilter::Trace, Config::default(), TerminalMode::Mixed, ColorChoice::Auto).expect("Logger error");
+
+
+    let dictionary = 0;
+    let keys = 1;
+    let index = 2;
+    let max = 3;
+    let item = 4;
+
     let program = Program {
         instructions: vec![
             // Create a dictionary with 3 key-value pairs
@@ -181,43 +195,53 @@ fn test_foreach_dictionary_loop() {
             Instruction::Push(Variant::String(String::from("key3"))),
             Instruction::Push(Variant::Integer(3)),
             Instruction::CreateDictionary(3),
-            Instruction::SetLocal(0),
+            Instruction::SetLocal(dictionary),
+
+            // Get the keys of the dictionary
+            Instruction::GetLocal(dictionary),
+            Instruction::GetDictionaryKeys,
+            Instruction::SetLocal(keys),
 
             // Set i = 0
-            Instruction::Push(Variant::Integer(0)),
-            Instruction::SetLocal(1),
+            Instruction::Push(Variant::Integer(0)), // 11
+            Instruction::SetLocal(index),
 
             // Get the length of the dictionary and store it in max
-            Instruction::GetLocal(0),
-            Instruction::GetDictionaryKeys,
+            Instruction::GetLocal(keys),
             Instruction::GetArrayLength,
-            Instruction::SetLocal(2),
+            Instruction::SetLocal(max),
 
             // while i < max
-            Instruction::GetLocal(1), // 14
-            Instruction::GetLocal(2),
+            Instruction::GetLocal(index), // 16
+            Instruction::GetLocal(max),
             Instruction::LessThan,
 
             // Jump if false
-            Instruction::JumpIfFalse(24),
+            Instruction::JumpIfFalse(31),
 
             // Get the dictionary
-            Instruction::GetLocal(0),
-            Instruction::GetLocal(1),
+            Instruction::GetLocal(dictionary),
+
+            // put dictionary item on stack
+            Instruction::GetLocal(keys),
+            Instruction::GetLocal(index),
+            Instruction::GetArrayItem,
+
+            // get dictionary item
             Instruction::GetDictionaryItem,
-            Instruction::SetLocal(3),
+            Instruction::SetLocal(item),
 
             // Increment i
-            Instruction::GetLocal(1),
+            Instruction::GetLocal(index),
             Instruction::Push(Variant::Integer(1)),
             Instruction::Add,
-            Instruction::SetLocal(1),
+            Instruction::SetLocal(index),
 
             // Jump to the beginning
-            Instruction::Jump(10),
+            Instruction::Jump(16),
 
             // Return i
-            Instruction::GetLocal(1), // 27
+            Instruction::GetLocal(index), // 31
             Instruction::Return
 
         ],
